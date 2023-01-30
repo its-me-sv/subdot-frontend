e45bd41e309923988da62c61fc095d1f3b4801ab
Suraj Vijay <surajcpp115@gmail.com>
2023-01-31 04:42:26 +0530

    fixed grid alignment at home page

--- src/pages/home/styles.tsx
+++ src/pages/home/styles.tsx
@@ -2,6 +2,6 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: grid;
-  grid-template-columns: 2fr 6fr 3fr;
+  grid-template-columns: 2fr 8fr 3fr;
   grid-template-rows: 1fr;
 `;
