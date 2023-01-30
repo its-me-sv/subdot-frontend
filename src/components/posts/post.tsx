0950be61003110377550b0b6f60156a9e50d3a61
Suraj Vijay <surajcpp115@gmail.com>
2023-01-30 05:31:23 +0530

    styled post card

--- src/components/posts/post.tsx
+++ src/components/posts/post.tsx
@@ -1,12 +1,53 @@
 import React from "react";
+import {format} from "timeago.js";
+
+import {FooterItem, PostContainer, PostContent, PostFooter, PostHeader, PostHeaderRight, PostImage, PostTime, PostUsername} from './styles';
 
 interface PostProps {}
 
 const Post: React.FC<PostProps> = () => {
     return (
-        <div>
-            Post
-        </div>
+        <PostContainer>
+            <PostHeader>
+                <img 
+                    alt="pp"
+                    src={require("../../assets/temp.jpg")} 
+                />
+                <PostHeaderRight>
+                    <PostUsername>{"<Dark Knight />"}</PostUsername>
+                    <PostTime>posted {format(new Date(2002, 4, 11))}</PostTime>
+                </PostHeaderRight>
+            </PostHeader>
+            <PostContent>
+                Hi there friends. I just took a picture with an stray dog
+            </PostContent>
+            <PostImage 
+                alt="content" 
+                src={require("../../assets/content_temp.jpg")} 
+            />
+            <PostFooter>
+                <FooterItem>
+                    <img 
+                        alt="like" 
+                        src={require("../../assets/icons/like.png")} 
+                    />
+                    <span>1.1m</span>
+                </FooterItem>
+                <FooterItem>
+                    <img 
+                        alt="comment" 
+                        src={require("../../assets/icons/comment.png")} 
+                    />
+                    <span>1.1m</span>
+                </FooterItem>
+                <FooterItem>
+                    <img 
+                        alt="tip"
+                        src={require("../../assets/icons/tip.png")} 
+                    />
+                </FooterItem>
+            </PostFooter>
+        </PostContainer>
     );
 };
 
--- src/components/posts/styles.tsx
+++ src/components/posts/styles.tsx
@@ -1,8 +1,10 @@
 import styled from "styled-components";
+import {BoxShadow} from "../../utils/styles";
 
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
+  overflow-y: scroll;
   margin: 0.42rem;
 `;
 
@@ -10,4 +12,82 @@ export const StickyButton = styled.div`
     align-self: flex-end;
     padding-top: 0.42rem;
     position: absolute;
+`;
+
+export const PostContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 36vw;
+  gap: 0.42rem;
+  padding: 0.42rem;
+  ${BoxShadow}
+`;
+
+export const PostHeader = styled.div`
+  display: flex;
+  align-self: flex-start;
+  align-items: center;
+  gap: 0.21rem;
+  img {
+    width: 3rem;
+    height: 3rem;
+    border-radius: 50%;
+  }
+  cursor: pointer;
+  &:hover {
+    opacity: 0.5;
+  }
+`;
+
+export const PostHeaderRight = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const PostUsername = styled.span`
+  font-family: Inter;
+  font-size: 1.2rem;
+  color: #1a1a1a;
+`;
+
+export const PostTime = styled.span`
+  font-family: Inter;
+  color: #1a1a1a;
+  opacity: 0.9;
+`;
+
+export const PostContent = styled.span`
+  font-family: Inter;
+  font-size: 1.2rem;
+  color: #1a1a1a;
+`;
+
+export const PostImage = styled.img`
+  width: 36vw;
+  border-radius: 0.14rem;
+`;
+
+export const PostFooter = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const FooterItem = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.14rem;
+  cursor: pointer;
+  img {
+    width: 2.1rem;
+    height: 2.1rem;
+  }
+  span {
+    font-family: Inter;
+    color: #1a1a1a;
+    font-size: 1.2rem;
+    opacity: 0.9;
+  }
+  &:hover {
+    opacity: 0.5;
+  }
 `;
