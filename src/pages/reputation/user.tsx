442ae8ad6809e06ea29b3d4d902f21968ba7f307
Suraj Vijay <surajcpp115@gmail.com>
2023-01-31 22:33:21 +0530

    navigation to profile from rp

--- src/pages/reputation/user.tsx
+++ src/pages/reputation/user.tsx
@@ -1,12 +1,15 @@
 import React from "react";
+import {useNavigate} from "react-router-dom";
 
 import {UserContainer} from "./styles";
 
 interface UserProps {}
 
 const User: React.FC<UserProps> = () => {
+    const navigate = useNavigate();
+
     return (
-      <UserContainer>
+      <UserContainer onClick={() => navigate("/profile/suraj")}>
         <img alt="pp" src={require("../../assets/temp.jpg")} />
         <span>{"<Dark Knight />"}</span>
         <span>200 RP</span>
