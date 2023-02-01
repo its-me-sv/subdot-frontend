59ce0f77219073b8ebde77f6b039c40ef43222e7
Suraj Vijay <surajcpp115@gmail.com>
2023-02-01 21:49:45 +0530

    moved reputation components from reputation page to components

--- src/pages/reputation/index.tsx
+++ src/pages/reputation/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-import {Container} from "./styles";
+import {Container} from "../../components/reputation/styles";
 
-import Info from "./info";
-import Board from "./board";
+import Info from "../../components/reputation/info";
+import Board from "../../components/reputation/board";
 
 interface ReputationPageProps {}
 
