cc3743a244c6343b0df052fe823f8014f5d559d8
Suraj Vijay <surajcpp115@gmail.com>
2023-02-01 22:48:33 +0530

    chat message component with styles

--- src/components/chat/messages.tsx
+++ src/components/chat/messages.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 
+import {MessagesContainer, Message} from "./styles";
+
 interface MessagesProps {}
 
 const Messages: React.FC<MessagesProps> = () => {
     return (
-        <div>
-            messages
-        </div>
+        <MessagesContainer>
+            {new Array(42).fill(7).map(() => (
+                <Message>
+                    <span>Hi there!! How ya doing?</span>
+                    <span>2:43 PM 27 Jan 2023</span>
+                </Message>
+            ))}
+        </MessagesContainer>
     );
 };
 
--- src/components/chat/styles.tsx
+++ src/components/chat/styles.tsx
@@ -12,16 +12,16 @@ export const InputContainer = styled.div`
   display: flex;
   ${BoxShadow}
   margin: 0.42rem;
-  gap: 0.14rem;
+  gap: 0.3rem;
   padding: 0.14rem;
   border-radius: 0.14rem;
   img {
-    width: 3rem;
+    width: 2.1rem;
     align-self: center;
   }
     textarea,
     textarea::placeholder {
-        width: 94%;
+        width: 95%;
         resize: none;
         outline: none;
         border: none;
@@ -31,4 +31,31 @@ export const InputContainer = styled.div`
         color: #1a1a1a;
         background-color: #d7d7d7;
     }
+`;
+
+export const MessagesContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.42rem;
+  max-height: 82vh;
+  overflow-y: scroll;
+  margin-right: 0.14rem;
+`;
+
+export const Message = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.42rem;
+  font-family: Inter;
+  font-size: 1.2rem;
+  color: #1a1a1a;
+  margin-right: 0.24rem;
+  background-color: #d7d7d7;
+  border-radius: 0.14rem;
+  padding: 0.14rem;
+  max-width: 21vw;
+  span:last-child {
+    align-self: flex-end;
+    font-size: 0.84rem;
+  }
 `;
