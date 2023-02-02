719d87dfd0fb59dcfab2b2d57d610379b16ffc90
Suraj Vijay <surajcpp115@gmail.com>
2023-02-02 10:00:26 +0530

    renamed app container

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-import App from './containers/App';
+
+import App from './containers/app';
 
 import {AppContextProvider} from './contexts/app';
 
