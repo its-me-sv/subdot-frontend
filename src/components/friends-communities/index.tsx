bac69749b6aa9ad01efb924ec09cea0eb70bf739
Suraj Vijay <surajcpp115@gmail.com>
2023-02-02 11:05:31 +0530

    added translations to friends and communities section

--- src/components/friends-communities/index.tsx
+++ src/components/friends-communities/index.tsx
@@ -2,16 +2,21 @@ import React from "react";
 
 import FrndCommSection from "./section";
 import {Container} from './styles';
+import {friends, communities} from "../../translations/frnds-comms";
+
+import {useAppContext} from "../../contexts/app";
 
 const dummyIds = new Array(42).fill("<Dark Knight />");
 
 interface FriendsCommunitiesProps {}
 
 const FriendsCommunites: React.FC<FriendsCommunitiesProps> = () => {
+    const {language} = useAppContext();
+
     return (
         <Container>
-            <FrndCommSection title="FRIENDS" ids={dummyIds} />
-            <FrndCommSection title="COMMUNITIES" ids={dummyIds} />
+            <FrndCommSection title={friends[language]} ids={dummyIds} />
+            <FrndCommSection title={communities[language]} ids={dummyIds} />
         </Container>
     );
 };
---
+++ src/translations/frnds-comms.ts
@@ -0,0 +1,15 @@
+export const friends: Array<string> = [
+    "FRIENDS",
+    "朋友们",
+    "दोस्त",
+    "AMIGOS",
+    "AMIS"
+];
+
+export const communities: Array<string> = [
+    "COMMUNITIES",
+    "社区",
+    "समुदाय",
+    "COMUNIDADES",
+    "COMMUNAUTÉS"
+];
