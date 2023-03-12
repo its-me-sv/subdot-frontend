fe3ac68128406dfb5123a2749297dd768fe99f59
Suraj Vijay <surajcpp115@gmail.com>
2023-03-12 19:18:46 +0530

    updated post styles

--- src/components/posts/styles.tsx
+++ src/components/posts/styles.tsx
@@ -25,7 +25,7 @@ export const PostContainer = styled.div<{dark: boolean;}>`
   flex-direction: column;
   width: 36vw;
   gap: 0.42rem;
-  padding: 0.42rem;
+  padding: 0.84rem;
   padding-bottom: 0.42rem;
   border-radius: 1.4rem;
   margin-bottom: 1.2rem;
