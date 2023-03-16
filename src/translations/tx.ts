d3b9da993e414a3e672a1d7a7ea40bcea19781da
Suraj Vijay <surajcpp115@gmail.com>
2023-03-16 22:41:29 +0530

    translations for tx decs

---
+++ src/translations/tx.ts
@@ -0,0 +1,72 @@
+const english: Array<string> = [
+    "Added comment to a post",
+    "Removed like from a post",
+    "Recieved Transfer / Tip from an user",
+    "Transfer / Tipped to an user",
+    "Created account",
+    "Advertisement cost",
+    "Liked a post",
+    "Updated profile",
+    "Shared a post",
+    "Followed a user"
+];
+
+const chinese: Array<string> = [
+    "在帖子中添加了评论",
+    "从帖子中删除了赞",
+    "收到用户的转账/小费",
+    "转移/给用户小费",
+    "创建帐户",
+    "广告费",
+    "喜欢一个帖子",
+    "更新资料",
+    "分享了一个帖子",
+    "关注了一个用户"
+];
+
+const hindi: Array<string> = [
+    "एक पोस्ट में टिप्पणी जोड़ी गई",
+    "लाइक को एक पोस्ट से हटा दिया गया",
+    "एक उपयोगकर्ता से प्राप्त स्थानांतरण/टिप",
+    "एक उपयोगकर्ता को स्थानांतरण / इत्तला दे दी",
+    "खाता बनाया",
+    "विज्ञापन लागत",
+    "एक पोस्ट पसंद आया",
+    "अद्यतन प्रोफ़ाइल",
+    "एक पोस्ट साझा की",
+    "एक उपयोगकर्ता का अनुसरण किया"
+];
+
+const spanish: Array<string> = [
+    "Comentario agregado a una publicación",
+    "Eliminado me gusta de una publicación",
+    "Transferencia / Consejo recibido de un usuari",
+    "Transferencia / Propina a un usuari",
+    "cuenta creada",
+    "costo de publicidad",
+    "Me gustó una publicación",
+    "Perfil actualizado",
+    "compartió una publicación",
+    "Seguido a un usuario"
+];
+
+const french: Array<string> = [
+    "Commentaire ajouté à une publication",
+    "J'aime supprimé d'une publication",
+    "Transfert reçu / Pourboire d'un utilisateur",
+    "Transférer / Transférer un pourboire à un utilisateur",
+    "Compte créé",
+    "Coût de la publicité",
+    "J'ai aimé un post",
+    "Profil mis à jour",
+    "A partagé une publication",
+    "A suivi un utilisateur"
+];
+
+export const txDesc: Array<Array<string>> = [
+    english,
+    chinese,
+    hindi,
+    spanish,
+    french
+];
