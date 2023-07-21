b869a13c79c753978403420949f2ee6559641cb5
Suraj Vijay <surajcpp115@gmail.com>
2023-07-22 02:35:16 +0530

    updated production backend url

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 export const REST_API: string = process.env.NODE_ENV === "development" 
 ? "http://192.168.29.97:5000/api"
-: "https://firm-affinity-378110.el.r.appspot.com/api";
+: "https://civil-cab-393407.el.r.appspot.com/api";
 
 export const SOCKET: string = REST_API.slice(0, -4);
 
